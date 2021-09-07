5f58a0365ef0c25454a7e37f2f7dc771cb0b5f6c
modos <modos.m98@gmail.com>
2021-09-07 08:23:37 +0430

    change basic

---
+++ server/requestHandler.js
@@ -0,0 +1,130 @@
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config.json')
+const crypto = require("crypto");
+const randomId = () => crypto.randomBytes(8).toString("hex")
+
+module.exports = async function requestHandler(req, res) {
+    
+    const headers = {
+        "Access-Control-Allow-Origin": "http://localhost:8080",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Allow-Headers": ['Content-Type', 'Authorization']
+      }
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204, headers);
+        res.end()
+        return
+      }
+
+    const uri = `mongodb+srv://modos:${config.db.password}@chatnodesocket.mdxoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    if (req.url === '/users' && req.method === 'POST') {
+
+        let body = ''
+        req.on('data', function(data) {
+        body += data
+
+        try {
+            const data = JSON.parse(body)
+            if (data.username && data.password && data.email) {
+                client.db(config.db.name).collection('users').find({ $or: [ { username:  data.username }, { email: data.email } ] }).toArray((err, result) => {
+
+                    if (!result.length > 0) {
+
+                        data.sessionID = randomId()
+
+                        client.db(config.db.name).collection('users').insertOne(data)
+
+                        res.writeHead(200, {
+                            "Access-Control-Allow-Origin": "http://localhost:8080",
+                             'Content-Type': 'application/json' })
+                        res.write(JSON.stringify({
+                             message: 'user signed up successfuly',
+                             sessionID: data.sessionID,
+                             username: data.username
+                            }))
+                        res.end()
+
+                    }else {
+                        res.writeHead(400, {
+                            "Access-Control-Allow-Origin": "http://localhost:8080",
+                             'Content-Type': 'application/json' })
+                        res.write(JSON.stringify({ message: 'user has already existed' }))
+                        res.end()
+                    }
+                })
+            }else {
+                res.writeHead(400, {
+                    "Access-Control-Allow-Origin": "http://localhost:8080",
+                     'Content-Type': 'application/json' })
+                res.write(JSON.stringify({ message: 'credentials are not correct' }))
+                res.end()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        })
+
+
+    }else if (req.url === '/users' && req.method === 'GET') {
+        const data = await client.db(config.db.name).collection('users').find({}).toArray()
+
+        res.writeHead(200, { 'Content-Type': 'application/json', 
+        "Access-Control-Allow-Origin": "http://localhost:8080"
+     })
+        res.write(JSON.stringify(data))
+        res.end()
+
+
+    }else if ( req.url === '/login' && req.method === 'POST') {
+        let body = ''
+        req.on('data', async function(data) {
+        body += data
+
+        try {
+            const data = JSON.parse(body)
+
+            if (data.username && data.password) {
+
+                const r = await client.db(config.db.name).collection('users').find({ $and: [ { username:  data.username }, { password: data.password } ] }).toArray()
+
+                if (r.length > 0) {
+                    res.writeHead(200, {
+                        "Access-Control-Allow-Origin": "http://localhost:8080",
+                         'Content-Type': 'application/json' })
+                    res.write(JSON.stringify({
+                         message: 'user Logged in successfuly',
+                         sessionID: r[0].sessionID,
+                         username: r[0].username
+                        }))
+                    res.end()
+                }else {
+                    res.writeHead(400, {
+                        "Access-Control-Allow-Origin": "http://localhost:8080",
+                         'Content-Type': 'application/json' })
+                    res.write(JSON.stringify({
+                         message: 'user does not exist or an internal error, try again',
+                        }))
+                    res.end()
+                }
+           }
+        
+        } catch (error) {
+            console.log(error)
+        }
+
+        })
+    }
+
+}
---
+++ src/components/SelectUsername.vue
@@ -0,0 +1,200 @@
+<template>
+  <div id="m-main">
+    <button v-on:click="trigger" class="triggerFormButton">{{ triggerForm }}</button>
+        <div class="form" v-if="isSign">
+            <form class="m-form" v-on:submit.prevent>
+                <div class="container">
+                  <span style="color: red;">{{ error }}</span>
+                  <h1 style="margin-bottom: 10px;">Sign Up</h1>
+                  <hr style="margin-bottom: 10px;"/>
+
+                  <label for="username"><b>Username</b></label>
+                  <input v-model="username" type="text" placeholder="Enter Username" name="username"  autocomplete="name">
+
+                  <label for="email"><b>Email</b></label>
+                  <input v-model="email" type="text" placeholder="Enter Email" name="email"  autocomplete="email">
+            
+                  <label for="psw"><b>Password</b></label>
+                  <input v-model="password" type="password" placeholder="Enter Password" name="password"  autocomplete="new-password">
+            
+                  <label for="psw-repeat"><b>Repeat Password</b></label>
+                  <input v-model="repeatPassword" type="password" placeholder="Repeat Password" name="psw-repeat"  autocomplete="new-password">
+                  
+                  <div>
+                    <button :disabled="isValid" v-on:click="onSubmit" type="submit" class="submitbtn">Sign Up</button>
+                  </div>
+
+                </div>
+              </form>
+        </div>
+
+               <div class="form" v-else>
+            <form class="m-form" v-on:submit.prevent>
+                <div class="container">
+                  <span style="color: red;">{{ error }}</span>
+                  <h1 style="margin-bottom: 10px;">Login</h1>
+                  <hr style="margin-bottom: 10px;"/>
+
+                  <label for="username"><b>Username</b></label>
+                  <input v-model="username" type="text" placeholder="Enter Username" name="username"  autocomplete="name">
+
+                  <label for="psw"><b>Password</b></label>
+                  <input v-model="password" type="password" placeholder="Enter Password" name="password"  autocomplete="new-password">
+
+                  <div>
+                    <button :disabled="isValidLogin" v-on:click="onLogin" type="submit" class="submitbtn">Login</button>
+                  </div>
+
+                </div>
+              </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "SelectUsername",
+  data() {
+    return {
+      isSign: true,
+      triggerForm: 'Login',
+      username: "",
+      password: "",
+      repeatPassword: "",
+      email: "",
+      error: ""
+    };
+  },
+  computed: {
+    isValid() {
+      return !(this.username.length > 2 && this.password !== "" && this.password === this.repeatPassword)
+    },
+    isValidLogin() {
+            return !(this.username.length > 2 && this.password !== "")
+    }
+
+  },
+  methods: {
+   async onLogin() {
+  const res = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: this.username,
+            password: this.password
+        })
+    })
+
+    const data = await res.json()
+
+
+    if (res.ok){
+        localStorage.setItem('sessionID', data.sessionID)
+      this.$emit("input", this.username);
+    }else {
+        this.error = "user does not exist or an internal error, try again"
+      }
+
+
+    },
+    trigger() {
+      this.triggerForm = this.triggerForm == "Login" ?  "Sign up" : "Login"
+      this.isSign = !this.isSign
+    },
+
+    async onSubmit() {
+      const res = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: this.username,
+            email: this.email,
+            password: this.password
+        })
+    })
+
+    const data = await res.json()
+
+
+    if (res.ok){
+        localStorage.setItem('sessionID', data.sessionID)
+      this.$emit("input", this.username);
+    }else {
+      if (data.message === "user has already existed") {
+        this.error = "user has already existed"
+      }
+    }
+
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+    margin: 0%;
+    padding: 0%;
+    box-sizing: border-box;
+}
+
+body {font-family: Arial, Helvetica, sans-serif;}
+
+#m-main {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    flex-flow: column;
+    align-items: center;
+}
+
+.form {
+    width: 50%;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border: none;
+    background: #f1f1f1;
+}
+input[type=text]:focus, input[type=password]:focus {
+    background-color: #ddd;
+    outline: none;
+}
+button {
+    background-color: #04AA6D;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 20%;
+    opacity: 0.9;
+}
+
+.triggerFormButton {
+  background-color:#044caa;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 20%;
+    opacity: 0.9;
+}
+  
+button:hover {
+    opacity:1;
+}
+
+button:disabled {
+  background-color: #ddd;
+}
+</style>
